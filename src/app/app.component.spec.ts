b2c18c9d6ee4e9fb78654596d30b63713cbdf8ac
BaptisteRenaudot <baptiste479@gmail.com>
2024-03-06 09:26:57 +0100

    fix solution

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'evalD2P2'`, () => {
+  it(`should have as title 'EvalD2P2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('evalD2P2');
+    expect(app.title).toEqual('EvalD2P2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('evalD2P2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('EvalD2P2 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'evalD2P2';
+  title = 'EvalD2P2';
 }
